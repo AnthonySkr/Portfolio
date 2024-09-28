41bd304a15d96ebcc0f22cd939c34e277aa6f16f
Anthony <skrant44@gmail.com>
2024-09-28 14:56:04 +0200

    Feat: Implement terminal contact functionality V1

--- js/side_bar.js
+++ js/side_bar.js
@@ -23,3 +23,45 @@ document.addEventListener("DOMContentLoaded", function () {
       });
    });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+   const terminalContact = document.querySelector(".terminal-contact");
+   const terminalOutput = document.getElementById("terminal-output");
+   const terminalClose = document.querySelector(".terminal-close");
+
+   function showTerminal(info) {
+      terminalOutput.textContent = "";
+      terminalContact.style.display = "block";
+      typeText(info);
+   }
+   function hideTerminal() {
+      terminalContact.style.display = "none";
+   }
+   function typeText(text) {
+      let index = 0;
+
+      function type() {
+         if (index < text.length) {
+            terminalOutput.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 100);
+         } else {
+            blinkingCursor.style.display = "none";
+         }
+      }
+
+      type();
+   }
+
+   document.getElementById("phone").addEventListener("click", function (event) {
+      event.preventDefault();
+      showTerminal(event.target.getAttribute("data-info"));
+   });
+
+   document.getElementById("mail").addEventListener("click", function (event) {
+      event.preventDefault();
+      showTerminal(event.target.getAttribute("data-info"));
+   });
+
+   terminalClose.addEventListener("click", hideTerminal);
+});

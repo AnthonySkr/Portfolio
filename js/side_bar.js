845aa73571c0389fc493175755ac16470300c9ac
Anthony <skrant44@gmail.com>
2024-10-10 10:40:34 +0200

    Refactor: Update terminal contact title and add animation

--- js/side_bar.js
+++ js/side_bar.js
@@ -1,6 +1,8 @@
+// Navigation latérale
 document.addEventListener("DOMContentLoaded", function () {
    const links = document.querySelectorAll("nav a");
 
+   // Fonction pour mettre à jour le lien actif
    function updateActiveLink() {
       links.forEach((link) => {
          if (link.href === window.location.href) {
@@ -11,32 +13,47 @@ document.addEventListener("DOMContentLoaded", function () {
       });
    }
 
+   // Mettre à jour le lien actif au chargement de la page
    updateActiveLink();
 
+   // Ajouter un événement de clic à chaque lien de navigation
    links.forEach((link) => {
       link.addEventListener("click", function (event) {
          event.preventDefault();
 
+         // Mettre à jour l'URL sans recharger la page
          window.history.pushState({}, "", link.href);
 
+         // Mettre à jour le lien actif
          updateActiveLink();
       });
    });
 });
 
+// Terminal contact
 document.addEventListener("DOMContentLoaded", function () {
    const terminalContact = document.querySelector(".terminal-contact");
    const terminalOutput = document.getElementById("terminal-output");
    const terminalClose = document.querySelector(".terminal-close");
+   const terminalTitle = document.querySelector(".terminal-title");
 
-   function showTerminal(info) {
+   // Fonction pour afficher le terminal avec le texte donné
+   function showTerminal(info, title) {
       terminalOutput.textContent = "";
+      terminalTitle.textContent = title;
       terminalContact.style.display = "block";
+      terminalContact.classList.add("show");
+
       typeText(info);
    }
+
+   // Fonction pour cacher le terminal
    function hideTerminal() {
       terminalContact.style.display = "none";
+      terminalContact.classList.remove("show");
    }
+
+   // Fonction pour taper le texte lettre par lettre
    function typeText(text) {
       let index = 0;
 
@@ -45,23 +62,24 @@ document.addEventListener("DOMContentLoaded", function () {
             terminalOutput.textContent += text.charAt(index);
             index++;
             setTimeout(type, 100);
-         } else {
-            blinkingCursor.style.display = "none";
          }
       }
 
       type();
    }
 
+   // Ajouter un événement de clic au lien avec l'ID 'phone'
    document.getElementById("phone").addEventListener("click", function (event) {
       event.preventDefault();
-      showTerminal(event.target.getAttribute("data-info"));
+      showTerminal(event.target.getAttribute("data-info"), "Téléphone");
    });
 
+   // Ajouter un événement de clic au lien avec l'ID 'mail'
    document.getElementById("mail").addEventListener("click", function (event) {
       event.preventDefault();
-      showTerminal(event.target.getAttribute("data-info"));
+      showTerminal(event.target.getAttribute("data-info"), "Email");
    });
 
+   // Ajouter un événement de clic au bouton de fermeture du terminal
    terminalClose.addEventListener("click", hideTerminal);
 });
